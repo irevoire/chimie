024e1805be398b0b6e475f79877a8bb47221787a
Tamo <irevoire@protonmail.ch>
2026-02-21 15:31:17 +0100

    add the memories route

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,4 @@
-use actix_web::web;
+use actix_web::{HttpRequest, HttpResponse, http::header::ContentType, web};
 
 mod auth;
 mod config;
@@ -12,5 +12,79 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("server").configure(server::configure))
         .service(web::scope("notifications").configure(notifications::configure))
         .service(web::scope("users").configure(users::configure))
-        .service(web::scope("auth").configure(auth::configure));
+        .service(web::scope("auth").configure(auth::configure))
+        .route("memories", web::get().to(memories));
+}
+
+#[derive(facet::Facet)]
+#[facet(transparent)]
+struct Memories(Vec<Memory>);
+
+#[derive(facet::Facet)]
+#[facet(rename_all = "camelCase", deny_unknown_fields)]
+struct Memory {
+    id: String,
+    created_at: String,
+    updated_at: String,
+    memory_at: String,
+    show_at: String,
+    hide_at: String,
+    owner_id: String,
+    #[facet(rename = "type")]
+    kind: String,
+    data: MemoryData,
+    is_saved: bool,
+
+    assets: Vec<Asset>,
+}
+
+#[derive(facet::Facet)]
+#[facet(rename_all = "camelCase", deny_unknown_fields)]
+struct MemoryData {
+    year: usize,
+}
+
+#[derive(facet::Facet)]
+#[facet(rename_all = "camelCase", deny_unknown_fields)]
+struct Asset {
+    id: String,
+    created_at: String,
+    device_asset_id: String,
+    owner_id: String,
+    device_id: String,
+    library_id: Option<String>,
+    #[facet(rename = "type")]
+    kind: String,
+    original_path: String,
+    original_file_name: String,
+    original_mime_type: String,
+    thumbhash: String,
+    file_created_at: String,
+    file_modified_at: String,
+    local_date_time: String,
+    updated_at: String,
+    is_favorite: bool,
+    is_archived: bool,
+    is_trashed: bool,
+    visibility: String,
+    duration: String,
+    live_photo_video_id: Option<String>,
+    // TODO: What is a people
+    people: Vec<()>,
+    checksum: String,
+    is_offline: bool,
+    has_metadata: bool,
+    duplicate_id: Option<String>,
+    resized: bool,
+    width: usize,
+    height: usize,
+    is_edited: bool,
+}
+
+pub async fn memories(_req: HttpRequest) -> HttpResponse {
+    let ret = Memories(Vec::new());
+    let ret = facet_json::to_vec(&ret).unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(ret)
 }
