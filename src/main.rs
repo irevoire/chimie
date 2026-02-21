cdcf95f557b5af677eeb45ca40e68433beece980
Tamo <irevoire@protonmail.ch>
2026-02-21 02:21:42 +0100

    Add the env_logger route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer, middleware::Logger, web};
 
 mod api;
 mod static_assets;
@@ -6,9 +6,12 @@ mod static_assets;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8080;
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     println!("Staring server on port {port}");
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(web::scope("api").configure(api::configure))
             .route("/{filename:.*}", web::get().to(static_assets::handle_files))
     })
